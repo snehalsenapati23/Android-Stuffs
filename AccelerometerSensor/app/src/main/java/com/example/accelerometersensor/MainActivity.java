f0527bb30aca5918b9d40867508bfaec5833bd01
Snehal <snehalsenapati07@gmail.com>
2022-06-07 17:49:59 +0530

    AccelerometerSensor Added

---
+++ AccelerometerSensor/app/src/main/java/com/example/accelerometersensor/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.accelerometersensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SensorManager sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        if(sensorManager!=null){
+            Sensor accse=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+             if (accse!=null){
+                 sensorManager.registerListener(this,accse,SensorManager.SENSOR_DELAY_NORMAL);
+             }
+        }
+        else {
+            Toast.makeText(this, "Sensor Service Not Detected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            ((TextView)findViewById(R.id.txt)).setText("X: "+event.values[0]+",Y: "+event.values[1]+", Z: "+event.values[2]);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
